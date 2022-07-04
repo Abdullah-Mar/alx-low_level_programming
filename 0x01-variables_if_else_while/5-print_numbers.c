589f4da3a5425a9f238d316000f590d6e58e51ce
Abdulrazaq <abdulrazaqm52@gmail.com>
2022-07-04 07:13:04 -0700

    print num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - entry poin
+ * 
+ * Return: always return 0
+ */
+int main(void)
+{
+int a = 0;
+for (; a < 10; a++)
+printf("%d", a);
+purchar('\n');
+return (0);
+}
